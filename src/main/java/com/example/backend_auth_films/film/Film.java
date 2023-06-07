7940cbc05fe47252ab5d5f00f4cf40e6782a192e
davidcarlin <david.carlin@barclays.com>
2023-06-07 02:04:40 +0100

    user entity, roles and permissions

--- src/main/java/com/example/backend_auth_films/film/Film.java
+++ src/main/java/com/example/backend_auth_films/film/Film.java
@@ -1,9 +1,6 @@
 package com.example.backend_auth_films.film;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,6 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Table(name = "films")
 public class Film {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
