d4aa9b9221e76be896cd513a433f5cf0682502a7
davidcarlin <david.carlin@barclays.com>
2023-06-07 01:27:13 +0100

    initial commit

---
+++ src/test/java/com/example/backend_auth_films/BackendAuthFilmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_auth_films;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAuthFilmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
